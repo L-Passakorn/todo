8f7506bebec34064fb5612fc42c39c3b93fd48d7
Synai Sunthorn <sinaisunthorn@gmail.com>
2023-05-25 13:19:27 +0700

    img url

--- mongodb-back/models/Activity.js
+++ mongodb-back/models/Activity.js
@@ -4,6 +4,7 @@ const activityScheme = new mongoose.Schema({
     title: { type: String, require: true},
     description: { type: String, required: true},
     status: { type: String, required: true},
+    imgUrl: {type: String}
   });
 
   activityScheme.statics.findByTitle = function (title) {
