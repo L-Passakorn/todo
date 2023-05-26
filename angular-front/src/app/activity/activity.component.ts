e1b28cfbf4a6bb514d4ffcd7762b0fa2df2d09bb
Passakorn Limseng <6510110356@email.psu.ac.th>
2023-05-26 15:17:14 +0700

    drag and drop broken pls fix right now

--- angular-front/src/app/activity/activity.component.ts
+++ angular-front/src/app/activity/activity.component.ts
@@ -10,8 +10,8 @@ import { OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
   
-  activities: Activity[] = [];
   item?: Activity
+  activities: Activity[] = [];
   planList: Activity[] = [];
   todoList: Activity[] = [];
   doneList: Activity[] = [];
@@ -19,28 +19,38 @@ export class ActivityComponent implements OnInit {
 
   ngOnInit(): void {
     this.getActivities();
+    this.getPlanList();
+    this.getTodoList();
+    this.getDoneList();
   }
 
   getActivities() {
     this.activityService.getActivities()
       .subscribe(activities => this.activities = activities);
   }
-  // getPlanList() {
-  //   this.activityService.getActivities()
-  //     .subscribe(activities => this.planList = activities);
-  // }
-  // getTodoList() {
-  //   this.activityService.getActivities()
-  //     .subscribe(activities => this.todoList = activities);
-  // }
-  // getDoneList() {
-  //   this.activityService.getActivities()
-  //     .subscribe(activities => this.doneList = activities);
-  // }
+  
+  getPlanList(){
+    this.activityService.getByStatus("planning")
+      .subscribe(planning => this.planList = planning);
+  }
+  getTodoList(){
+    this.activityService.getByStatus("todo")
+      .subscribe(todo => this.todoList = todo);
+  }
+  getDoneList(){
+    this.activityService.getByStatus("done")
+      .subscribe(done => this.doneList = done);
+  }
 
   updateActivity() {
     if (this.item) {
-      this.activityService.updateActivity(this.item._id, this.item)
+      this.activityService.updateActivity(this.item).subscribe(()=>{this.getActivities(),this.item=undefined})
     }
   }
+  updateStatus(status: string) {
+    if (this.item) {
+      this.activityService.updateActivity({status: status}).subscribe(()=>{this.getActivities(),this.item=undefined})
+    }
+  }
+
 }
